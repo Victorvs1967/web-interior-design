11364bed78a59320062845c4c08558e213e53591
Victorvs1967 <victorsmirnov@me.com>
2023-08-04 17:25:18 +0300

    translate to english

--- task/clear.js
+++ task/clear.js
@@ -3,6 +3,6 @@ import { deleteAsync } from 'del';
 
 import path from '../config/path.js';
 
-const clear = async () => await deleteAsync([path.root.concat('/index.html'), path.sass.dest, path.js.dest]);
+const clear = async () => await deleteAsync[path.root.concat('/index.html'), path.sass.dest, path.js.dest];
 
 export default clear;
